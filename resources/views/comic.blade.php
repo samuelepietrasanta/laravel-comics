1aa29088e6f11c42ea15bf4897abfdd2a522e6aa
samuelepietrasanta <samuele.pietrasanta@gmail.com>
2021-11-02 17:05:44 +0100

    changed css of the website

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,10 +1,19 @@
-@extends("layouts.main");
+@extends("layouts.main")
 
 @section("contenuto")
 
-<h1>{{$comics['title']}}</h1>
-    <img src="{{$comics['thumb']}}" alt="immagine comic">
-
-@dump($comics)
+    <h1 class="comic_title">{{$comics['title']}}</h1>
+    <div class="single_comic_price">
+        <h3>Price: {{$comics['price']}}</h3>
+    </div>
+    <div class="single_comic_section">
+        <div class="single_comic_image">
+            <img src="{{$comics['thumb']}}" alt="immagine comic">
+        </div>
+        <div class="single_comic_description">
+            <p>{{$comics['description']}}</p>
+            <button>BUY NOW</button>
+        </div>
+    </div>
 
 @endsection
--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -2,18 +2,28 @@
 
 @section("contenuto")
 
+<div class="cards-container">
+
+
+
 @foreach ($fumetti as $fumetto)
 
-<div class="comics_card">
+    <div class="comics_card">
 
-    <a class="link_fumetto" href='{{url("/comic/$loop->index")}}'>
-        {{$fumetto['title']}}
-    </a>
+        <a class="link_fumetto" href='{{url("/comic/$loop->index")}}'>
+            <h4>{{$fumetto['title']}}</h4>
+        </a>
+        <a class="immagine_fumetto" href='{{url("/comic/$loop->index")}}'>
+            <img src="{{$fumetto['thumb']}}" alt="error">
+        </a>
 
-</div>
+    </div>
 
 
 @endforeach
+
+</div>
+
 
 
 @endsection
